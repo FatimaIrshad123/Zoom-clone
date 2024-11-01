5708a4d69a7cb98aa79d81faa96665632b4067fc
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-11-01 19:37:08 +0500

    Adding video stream functionality

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -5,22 +5,58 @@ import HomeCard from './HomeCard'
 import { useRouter } from 'next/navigation'
 import MeetingModal from './MeetingModal'
 import { useUser } from '@clerk/nextjs'
-import { useStreamVideoClient } from '@stream-io/video-react-sdk'
+import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk'
+import { useToast } from "@/hooks/use-toast"
 
 const MeetingTypeList = () => {
     const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>(undefined)
+    const [values, setValues] = useState({
+      dateTime: new Date(),
+      description: '',
+      link: ''
+    })
+    const [callDetails, setCallDetails] = useState<Call>();
 
     const {user} = useUser();
     const client = useStreamVideoClient();
+    const { toast } = useToast()
 
-    const createMeeting = () => {
+    const createMeeting = async () => {
       if(!client || !user) return ;
 
       try {
+        if (!values.dateTime){
+          toast({title:'Please select date and time'})
+          return;
+        }
+
         const id = crypto.randomUUID();
+        const call = client.call('default', id);
+
+        if(!call) throw new Error('Failed to create call')
         
+          const startAt = values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+          const description = values.description || 'Instant meeting';
+
+          await call.getOrCreate({
+            data: {
+              starts_at: startAt,
+              custom: {
+                description
+              }
+            }
+          })
+          setCallDetails(call);
+
+          if (!values.description){
+            router.push(`meeting/${call.id}`)
+          }
+          toast({title: 'Meeting Created'})
       }catch(error){
         console.log(error)
+        toast({
+          title: "Failed to create meeting",
+        })
       }
     };
 
